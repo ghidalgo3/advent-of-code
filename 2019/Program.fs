8a9c82f413601688a65d8ec87806a5143cdee100
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2022-09-19 20:22:24 -0400

    day 1 part 1

--- 2019/Program.fs
+++ 2019/Program.fs
@@ -1,4 +1,13 @@
-﻿
+﻿open System.IO
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let fuelRequirement mass = mass / 3 - 2
+
+let day1 (input : string) : int =
+    let input = [ for line in File.ReadLines(input) -> line |> int |> fuelRequirement ]
+    input |> List.sum
+
+[<EntryPoint>]
+let main args =
+    let one : int = day1 args.[0]
+    printfn $"{one}" 
+    0
