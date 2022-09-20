8cea18cbaf478e4d497182a2d861fcdba647d142
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2022-09-19 21:52:46 -0400

    day 2 part 1

---
+++ 2019/Intcode.fs
@@ -0,0 +1,37 @@
+module Intcode
+
+type Program = {
+    Memory : int array
+    PC : int }
+
+type Instruction =
+| Add of int * int * int
+| Mult of int * int * int
+| Halt
+| Unknown
+
+let decode program =
+    match program.Memory.[program.PC] with
+    | 1 -> Add(program.Memory.[program.PC + 1], program.Memory.[program.PC + 2], program.Memory.[program.PC + 3])
+    | 2 -> Mult(program.Memory.[program.PC + 1], program.Memory.[program.PC + 2], program.Memory.[program.PC + 3])
+    | 99 -> Halt
+    | _ -> Unknown
+
+let eval program =
+    match decode program with
+    | Add(r0, r1, r2) ->
+        let a = program.Memory.[r0]
+        let b = program.Memory.[r1]
+        program.Memory.[r2] <- a + b
+        { program with PC = program.PC + 4 }
+    | Mult(r0, r1, r2) ->
+        let a = program.Memory.[r0]
+        let b = program.Memory.[r1]
+        program.Memory.[r2] <- a * b
+        { program with PC = program.PC + 4 }
+    | _ -> program
+
+let rec run program =
+    match decode program with 
+    | Halt -> program
+    | _ -> (eval >> run) program
--- 2019/Program.fs
+++ 2019/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System.IO
+open Intcode
 
 let rec fuelRequirement mass =
     let req = mass / 3 - 2
@@ -13,8 +14,16 @@ let day1 (input : string) : int =
             line |> int |> fuelRequirement ]
     input |> List.sum
 
-[<EntryPoint>]
-let main args =
-    let one : int = day1 args.[0]
-    printfn $"{one}" 
-    0
+let day2 input =
+    let input = [| for n in File.ReadAllText(input).Split(",") -> int n |]
+    input.[1] <- 12
+    input.[2] <- 2
+    let program = { Memory = input ; PC = 0 }
+    let finalState = run program
+    finalState.Memory.[0]
+
+
+let one : int = day1 "input/1"
+let two : int = day2 "input/2"
+printfn $"Day 1-2: {one}" 
+printfn $"Day 2-1: {two}" 
