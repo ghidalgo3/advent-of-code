2603c019370321f657dc586d393d47f16e973564
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2022-09-19 20:30:49 -0400

    day 1 part 2

--- 2019/Program.fs
+++ 2019/Program.fs
@@ -1,9 +1,16 @@
 ﻿open System.IO
 
-let fuelRequirement mass = mass / 3 - 2
+let rec fuelRequirement mass =
+    let req = mass / 3 - 2
+    if req > 0 then
+        req + fuelRequirement req
+    else 
+        0
 
 let day1 (input : string) : int =
-    let input = [ for line in File.ReadLines(input) -> line |> int |> fuelRequirement ]
+    let input = [
+        for line in File.ReadLines(input) ->
+            line |> int |> fuelRequirement ]
     input |> List.sum
 
 [<EntryPoint>]
