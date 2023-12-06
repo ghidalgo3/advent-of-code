04a7222a22dc5167e55f0b565c6e4778aeaeecb0
Gustavo Hidalgo Zambrano <gustavo@Gustavos-MBP.attlocal.net>
2023-12-05 20:40:59 -0500

    day 1 part 1

---
+++ 2023/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int GetCalibrationValue(string text)
+{
+    List<int> numbers = new();
+    foreach (char c in text)
+    {
+        if (c >= '0' && c <= '9')
+        {
+            numbers.Add(c - '0');
+        }
+    }
+    if (numbers.Count == 1)
+    {
+        return numbers[0] * 10 + numbers[0];
+    }
+    else
+    {
+        return numbers[0] * 10 + numbers[numbers.Count - 1];
+    }
+}
+
+Console.WriteLine(File.ReadAllLines("input/1").Select(GetCalibrationValue).Sum());
